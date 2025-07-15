426c8d19aaef3304e489a03253a4bf21ba049572
ikuto_murakami <m.ikuto@icloud.com>
2025-07-15 22:36:38 +0900

    画面遷移を追加 タイトル画面、ゲームオーバー画面を追加 タイトル画面からスペースキーを押下でゲームプレイ画面へ遷移する処理を追加 ボールが画面下に達したら、ゲームプレイ画面からゲームオーバー画面へ遷移する処理を追加 ゲームオーバー画面から5秒経過すると、タイトル画面へ遷移する処理を追加

--- TennisGame.cpp
+++ TennisGame.cpp
@@ -26,42 +26,86 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int racketW = 120;
 	int racketH = 12;
 
+	// ゲーム進行に関する変数、スコアを代入する変数
+	enum { TITLE, PLAY, OVER };
+	int scene = TITLE;
+	int timer = 0;
 	int score = 0; // スコアを代入
 	int highScore = 1000; // ハイスコアを代入
+	int dx, dy; // ヒットチェック用の変数宣言
 
 	while (1) // メインループ
 	{
 		ClearDrawScreen(); // 画面をクリアする
-		// ボールの処理
-		ballX = ballX + ballVx;
-		if (ballX < ballR && ballVx < 0) ballVx = -ballVx;
-		if (ballX > WIDTH - ballR && ballVx > 0) ballVx = -ballVx;
-		ballY = ballY + ballVy;
-		if (ballY < ballR && ballVy < 0) ballVy = -ballVy;
-		if (ballY > HEIGHT - ballR && ballVy > 0) ballVy = -ballVy;
-		DrawCircle(ballX, ballY, ballR, 0xff0000, TRUE); // ボール
+		timer++;
 
-		// ラケットの処理
-		if (CheckHitKey(KEY_INPUT_LEFT) == 1) // 左キー押し下し
+		switch (scene)
 		{
-			racketX = racketX - 10;
-			if (racketX < racketW / 2) racketX = racketW / 2;
-		}
-		if (CheckHitKey(KEY_INPUT_RIGHT) == 1) // 右キー押し下し
-		{
-			racketX = racketX + 10;
-			if (racketX > WIDTH - racketW / 2) racketX = WIDTH - racketW / 2;
-		}
-		DrawBox(racketX - racketW / 2, racketY - racketH / 2, racketX + racketW / 2, racketY + racketH / 2, 0x0080ff, TRUE); //ラケット
+		case TITLE: // タイトル画面の処理
+			SetFontSize(50);
+			DrawString(WIDTH / 2 - 50 / 2 * 12 / 2, HEIGHT / 3, "Tennis Game", 0x00ff00);
+			if (timer % 60 < 30) { //文字を点滅表示
+				SetFontSize(30);
+				DrawString(WIDTH / 2 - 30 / 2 * 21 / 2, HEIGHT * 2 / 3, "Press SPACE to start", 0x00ffff);
+			}
+			if (CheckHitKey(KEY_INPUT_SPACE) == 1) // スペースキー押し下し
+			{
+				ballX = 40;
+				ballY = 80;
+				ballVx = 5;
+				ballVy = 5;
+				racketX = WIDTH / 2;
+				racketY = HEIGHT - 50;
+				score = 0;
+				scene = PLAY;
+			}
+			break;
 
-		// ヒットチェック
-		int dx = ballX - racketX; // x軸方向の距離
-		int dy = ballY - racketY; // y軸方向の距離
-		if (-racketW / 2 - 10 < dx && dx < racketW / 2 + 10 && -20 < dy && dy < 0)
-		{
-			ballVy = -5 - rand() % 5;
-			score = score + 100;
-			if (score > highScore) highScore = score; // ハイスコアの更新
+		case PLAY: // ゲームをプレイする処理
+			// ボールの処理
+			ballX = ballX + ballVx;
+			if (ballX < ballR && ballVx < 0) ballVx = -ballVx;
+			if (ballX > WIDTH - ballR && ballVx > 0) ballVx = -ballVx;
+			ballY = ballY + ballVy;
+			if (ballY < ballR && ballVy < 0) ballVy = -ballVy;
+			// if (ballY > HEIGHT - ballR && ballVy > 0) ballVy = -ballVy;
+			if (ballY > HEIGHT) // ボールが下端に達した時
+			{
+				scene = OVER;
+				timer = 0;
+				break;
+			}
+			DrawCircle(ballX, ballY, ballR, 0xff0000, TRUE); // ボール
+
+			// ラケットの処理
+			if (CheckHitKey(KEY_INPUT_LEFT) == 1) // 左キー押し下し
+			{
+				racketX = racketX - 10;
+				if (racketX < racketW / 2) racketX = racketW / 2;
+			}
+			if (CheckHitKey(KEY_INPUT_RIGHT) == 1) // 右キー押し下し
+			{
+				racketX = racketX + 10;
+				if (racketX > WIDTH - racketW / 2) racketX = WIDTH - racketW / 2;
+			}
+			DrawBox(racketX - racketW / 2, racketY - racketH / 2, racketX + racketW / 2, racketY + racketH / 2, 0x0080ff, TRUE); //ラケット
+
+			// ヒットチェック
+			dx = ballX - racketX; // x軸方向の距離
+			dy = ballY - racketY; // y軸方向の距離
+			if (-racketW / 2 - 10 < dx && dx < racketW / 2 + 10 && -20 < dy && dy < 0)
+			{
+				ballVy = -5 - rand() % 5;
+				score = score + 100;
+				if (score > highScore) highScore = score; // ハイスコアの更新
+			}
+			break;
+
+		case OVER: // ゲームオーバーの処理
+			SetFontSize(40);
+			DrawString(WIDTH / 2 - 40 / 2 * 9 /2, HEIGHT / 3, "GAME OVER", 0xff0000);
+			if (timer > 60 * 5) scene = TITLE;
+			break;
 		}
 
 		SetFontSize(30); // スコアとハイスコアの文字の大きさ
